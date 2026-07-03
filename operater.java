45afd8286e4d5d1c1bfc28d81a5e09eecb213ddf
Jothisree Annadurai <jothisreeannadurai@gmail.com>
2026-07-03 18:17:30 +0530

    Add files via upload
    practiced java basics operatos concept day-5

---
+++ operater.java
@@ -0,0 +1,22 @@
+import java.util.Scanner();
+package practice_java;
+
+public class operater {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner=new scanner(system.in);
+		int a,b,c;
+		System.out.println("enter a number:a");
+		System.out.println("enter a number:b");
+		 int number=scanner.nextInt();
+		 int number2=scanner.nextInt();
+		System.out.println("number");
+ 
+		
+		
+
+	}
+
+		
+	}
