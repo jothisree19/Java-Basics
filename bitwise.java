7803b073291609b83e63bbe42d92c7a861083dcf
Jothisree Annadurai <jothisreeannadurai@gmail.com>
2026-07-03 18:07:41 +0530

    Add files via upload
    practiced java basics bitwise concept day-2

---
+++ bitwise.java
@@ -0,0 +1,22 @@
+package practice_java;
+
+public class bitwise {
+
+	public static void main(String[] args) {
+		
+		int a=5,b=9;
+		int c=(a<<b);
+		System.out.println("a&b="+c);
+		 c=a|b;
+		System.out.println("a|b="+c);
+		  c=~a;
+		System.out.println("~a="+c);
+
+		
+
+		
+
+
+	}
+
+}
