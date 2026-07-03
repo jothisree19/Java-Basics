1eded6466d0cc3eac03fed474e783826188db8ba
Jothisree Annadurai <jothisreeannadurai@gmail.com>
2026-07-03 18:21:33 +0530

    Add files via upload
    practiced java basics Switchcase example (2) concept day-8

---
+++ switc.java
@@ -0,0 +1,63 @@
+package practice_java;
+
+import java.util.Scanner;
+
+public class switc {
+
+	public static void main(String[] args) {
+		int a,b;
+		
+		Scanner Scanner = new Scanner(System.in);
+		System.out.println("enter a number");
+		 a=Scanner.nextInt();
+		System.out.println("enter b value");
+		 b=Scanner.nextInt();
+		char operator;
+		System.out.println("enter a operator +,-,*,/ or %");
+		operator=Scanner.next().charAt(0);
+		
+		switch(operator) {
+		case'+':
+			System.out.println(a + "+" + b + "="+ (a+b));
+			break;
+		case'-':
+			System.out.println(a + "-" + b + "="+ (a-b));
+			break;
+
+		case'*':
+			System.out.println(a + "*" + b + "="+ (a*b));
+			break;
+
+		case'%':
+			System.out.println(a + "%" + b + "="+ (a%b));
+			break;
+
+		case'/':
+			System.out.println(a + "/" + b + "="+ (a/b));
+			break;
+			
+			default:
+				System.out.println("invalid");
+		}
+	}
+
+
+				
+
+
+
+
+			
+		
+
+			
+			
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
